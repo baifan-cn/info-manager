3a9c010bbfad58b3ba6f2141ef13023dc00659c2
fan.bai <fan.bai@luduo.co>
2025-10-30 01:19:22 +0800

    refactor: 移除未使用的defineEmits导入

--- info-manager-front/src/components/common/RichTextEditor.vue
+++ info-manager-front/src/components/common/RichTextEditor.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, defineEmits, defineProps, onBeforeUnmount, onMounted, ref, watch } from 'vue'
+import { computed, onBeforeUnmount, onMounted, ref, watch } from 'vue'
 
 const props = defineProps<{
   modelValue: string
